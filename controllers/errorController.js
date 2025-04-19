ac4571e0dc20073404cdc24356757b7597952ae9
njokojunior <njokojunior1@gmail.com>
2025-04-19 18:25:46 +0200

    Added validation error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,21 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.keyValue && Object.values(err.keyValue)[0];
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(val => val.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -19,7 +37,6 @@ const sendErrorProd = (err, res) => {
   else {
     // 1. Log err to console for dev
     console.error('ERROR 💥', err);
-
     // Send generic message
     res.status(500).json({
       status: 'error',
@@ -29,15 +46,24 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
-  // 1. create middleware
-  // 2. create error
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    console.log(err);
+    // When you're using the spread operator to create a copy of the error object, it's only copying the enumerable properties. Important properties like name might be lost in this process, especially if they're on the prototype chain rather than the error object itself.
+    // let error = { ...err };
+
+    // New updated code
+    let error = Object.create(Object.getPrototypeOf(err));
+    Object.assign(error, err);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
