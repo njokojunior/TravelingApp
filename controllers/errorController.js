7e0a6af26662cd9e7845d6e85257f447d4e462cc
njokojunior <njokojunior1@gmail.com>
2025-04-19 06:50:04 +0200

    Added custom operational errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,33 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  }
+
+  // Programming or unknown error, not to leak error details
+  else {
+    // 1. Log err to console for dev
+    console.error('ERROR 💥', err);
+
+    // Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err.stack);
   // 1. create middleware
@@ -5,8 +35,9 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
