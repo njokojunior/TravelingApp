d54723101d3e1e372e97ca9a9f60c446d16e73df
njokojunior <njokojunior1@gmail.com>
2025-04-19 18:23:08 +0200

    Added unhandle exception and uncaught exception handlers

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION 💥 Shutting Down');
+  console.log(err.name, err.message);
+  process.exit(1); //1 stands for uncaught exception
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-
-mongoose.connect(DB).then(() =>
-  console.log('DB connection Succesful')
-).catch((err) => console.log(err))
-
+mongoose.connect(DB).then(() => console.log('DB connection Succesful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION 💥 Shutting Down');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1); //1 stands for uncaught exception
+  });
 });
