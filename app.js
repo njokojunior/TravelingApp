7f4b033674f0c7251bde7e50959c41e5ee5b1897
njokojunior <njokojunior1@gmail.com>
2025-04-21 06:41:21 +0200

    Implemented data sanitization

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+// 1) GLOBAL MIDDLEWARES
 const app = express();
 
-// 1) GLOBAL MIDDLEWARES
+//Set Security HTTP headers
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// Limit request from same API
 const limiter = rateLimit({
   max: 100,
   windowMS: 60 * 60 * 1000,
@@ -22,9 +29,21 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the data into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization agains nosql query injection
+// It will remove all the dollar signs in our body
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+// I will clean any malicious code by converting all the html symbols added by an hacker
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middlewares
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
