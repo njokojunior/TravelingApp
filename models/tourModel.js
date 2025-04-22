47750460d27b3fd715fc7c37286eeeaf448ca4ed
njokojunior <njokojunior1@gmail.com>
2025-04-22 14:39:17 +0200

    Embedded tours and users for tours management

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -112,7 +113,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -129,10 +131,10 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-// tourSchema.post('save', function(doc,next){
-//   console.log(doc)
-//   next();
-// })
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+});
 
 // Query Middleware
 
